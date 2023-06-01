1b0bdce75fe2ff5805ca92cdd4f29a19b40ef93b
Henry Jia <Henry.Jia@grapecity.com>
2023-06-01 09:11:13 +0000

    Add type and use [] to replace {}

--- json/minify.js
+++ json/minify.js
@@ -3,14 +3,26 @@ const SpecialValueDictionary = {
     undefined: "\x04"
 };
 
+/** 
+     * @template T
+*/
 class ValueIndexMap {
+    /** @type T[] */
     #unique = [];
+    /** @type Object.<string, number> */
     #indexMap = {};
-
+    /** 
+     * @param {T} value
+     * @return {string}
+    */
     #getKeyByValue(value) {
         return value?.toString();
     }
 
+    /** 
+     * @param {T} value
+     * @return {string}
+    */
     getKey(value) {
         return value == null ? SpecialValueDictionary[value] : this.#getKeyByValue(value);
     }
@@ -19,6 +31,9 @@ class ValueIndexMap {
         return Object.keys(this.#indexMap);
     }
 
+    /** 
+     * @param {T} value
+    */
     push(value) {
         let key = this.getKey(value);
         let index = this.#indexMap[key];
@@ -30,6 +45,9 @@ class ValueIndexMap {
         return index;
     }
 
+    /** 
+     * @param {number} index
+    */
     getValueByIndex(index) {
         return this.#unique[index];
     }
@@ -38,20 +56,36 @@ class ValueIndexMap {
         return this.#unique.slice();
     }
 
+    /** 
+     * @param {T[]} values
+    */
     adjustValues(values) {
         return values.map((v) => this.push(v));
     }
 
+    /** 
+     * @param {T[]} values
+    */
     addValues(values) {
         values.forEach((v) => this.push(v));
     }
 
+    /** 
+     * @param {T} value
+    */
     getIndexOfValue(value) {
         let key = this.getKey(value);
         return this.#indexMap[key];
     }
 }
 
+/**
+ * @template T
+ * @param {T[]|Object.<string, T>} target 
+ * @param {string|number} key 
+ * @param {T} defaultValue 
+  * @returns {T}
+ */
 function ensureObject(target, key, defaultValue) {
     if (!target[key]) {
         target[key] = defaultValue;
@@ -59,11 +93,19 @@ function ensureObject(target, key, defaultValue) {
     return target[key];
 }
 
+/**
+ * 
+ * @param {Object.<string, any>[]} dataArray 
+ * @returns 
+ */
 function miniArray(dataArray) {
+    /** @type Object.<string, ValueIndexMap<any>> */
     const map = {};
+    /** @type ValueIndexMap<string>> */
     const propMap = new ValueIndexMap();
     const data = dataArray.map((v) => {
-        const result = {};
+        /** @type number[] */
+        const result = [];
         for(const key in v) {
             const index = propMap.push(key);
             const valueMap = ensureObject(map, index, new ValueIndexMap());
@@ -72,9 +114,10 @@ function miniArray(dataArray) {
         return result;
     });
     const restoreData = data.map(v => {
+        /** @type Object.<string, any> */
         const d = {}; 
-        for(const key in v) { 
-            d[propMap.getValueByIndex(+key)] = map[+key].getValueByIndex(+v[key]);
+        for(const key in v) {
+            d[propMap.getValueByIndex(+key)] = map[key].getValueByIndex(v[key]);
         }
         return d;
     });
